9b21579c64da5f732ba8229b2dfb15289a2a24a5
Mohd Arsh <mohd.arsh5344266@gmail.com>
2026-07-26 19:13:05 +0530

    registering user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,67 @@
 import { User } from "../models/user.model.js";
 import { apiResponse } from "../utils/api-response.js";
-import { apiError } from "../utils/api-error.js";
+import { ApiError, apiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import { sendEmail } from "../utils/mail.js";
+
+
+
+
+const registerUser = asyncHandler(async(req, res) => {
+    const { email, username, password, role } = req.body;
+    
+    const excitedUser = await User.findone({
+        $or: [{ username }, { emaill }]
+
+    });
+
+    if (excitedUser) {
+        throw new ApiError(409,"user with email or username already excited")
+    }
+
+    const user = await User.create({
+        email,
+        password,
+        username,
+        isEmailVerified: false
+    });
+
+    const { unHashedToken, hashedtoken, tokenExpiry } =
+        user.generateTemporaryToken();
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationExpiry = tokenExpiry;
+    await user.save({ validateBeforeSave: false });
+
+    await sendEmail(
+        {
+            email: user?.email,
+            subject: "please verify your email",
+            mailgenContent: emailVerificationMailgenContent(
+                user.username,
+                `${req.protoco}://${req.get("host")}/api/v1//user/verify-email/${unHashedToken}`
+            ),
+        }
+    );
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+
+    );
+
+    if (!createdUser) {
+        throw new ApiError(500, "something went wrong while registering a user");
+    }
+    
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                200,
+                { user: createdUser },
+                "user registered successfully and verification email has been sent to your email"
+            )
+        )
+    
+
+    
+});
