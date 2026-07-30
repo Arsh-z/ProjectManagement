7d67ab794ca520de0a329270adfd38295a292934
Mohd Arsh <mohd.arsh5344266@gmail.com>
2026-07-30 21:46:33 +0530

    adding logout

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { User, user } from "..//models/user.model.js";
+import { User } from "..//models/user.model.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import jwt from "jsonwebtoken";
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { login, registerUser } from "../controllers/auth.controller.js";
+import { login, logoutUser, registerUser } from "../controllers/auth.controller.js";
 
 import { validate } from "../middlewares/validator.middleware.js";
 
 import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
 
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
 router.route('/register').post(userRegisterValidator(), validate ,registerUser);
-router.route("/login").post(userLoginValidator(),validate, login);
+router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/logout").post(logoutUser);
 export default router;
